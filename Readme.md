5495ce0e0ad89a1d9195ee43b209e5c14794c009
Tuur123 <arthur.dhooge@outlook.com>
2021-11-02 17:54:54 +0100

    automated setup proces

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Setup
+
+Run this command to setup venv and install python modules
+
+>Powershell.exe -executionpolicy remotesigned -File  .\init.ps1
--- init.ps1
+++ init.ps1
@@ -1,4 +1,3 @@
 py -m venv .
-Scripts\activate.ps1
 Scripts\python.exe -m pip install --upgrade pip
-pip install -r requirements.txt
+Scripts\python.exe -m pip install -r requirements.txt
