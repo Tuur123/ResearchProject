a24f123a791cced604e9156d9cc9a9c26340e0c6
Arthur d'Hooge <arthur.dhooge@outlook.com>
2021-11-02 15:39:33 +0100

    stream test

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from djitellopy import tello
+from threading import thread
+import cv2
 
 tello = tello.Tello()
+
+tello.connect()
+tello.streamon()
+
+def displaySteam(tello):
+    img = tello.get_frame_read().frame
+    cv2.imshow("Stream", img)
+    cv2.waitkey(1)
 
